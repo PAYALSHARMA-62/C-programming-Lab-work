7e40f016f6a92c03365bbeda59e64b0e9cbd2efe
PAYALSHARMA-62 <146845595+PAYALSHARMA-62@users.noreply.github.com>
2023-11-19 16:42:02 +0530

    Add files via upload

---
+++ 1.2 Program to print hello world by using new line character.c
@@ -0,0 +1,10 @@
+// Title of program
+//Program to print hello world by using new line character
+//Header file
+#include<stdio.h>  
+//function heading
+void main()
+{
+//output statement
+printf("helloworld\n helloworld");
+}
