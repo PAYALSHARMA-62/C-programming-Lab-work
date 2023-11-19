0bcded3f528523ac97039f360eecc6178a455f40
PAYALSHARMA-62 <146845595+PAYALSHARMA-62@users.noreply.github.com>
2023-11-19 16:46:07 +0530

    Add files via upload

---
+++ 3.1 Program to check if a number is even or odd.c
@@ -0,0 +1,26 @@
+// title of program
+// Program to check if a number is even or odd
+// header file
+#include<stdio.h>
+// function heading
+void main()
+{
+// variable declaration
+int a;
+// output statement
+printf("enter the number");
+// input statement
+scanf("%d",&a);
+// condition of if else
+if(a%2==0)
+{
+// output statement
+printf("even");
+}
+// condition of if else
+else
+{
+// output statement
+printf("odd");
+}
+}
