057c16d3eeac4d5ef7b7e71ae9e4dab6a9706085
PAYALSHARMA-62 <146845595+PAYALSHARMA-62@users.noreply.github.com>
2023-11-19 16:43:33 +0530

    Add files via upload

---
+++ 2.1 Program for arithmetic operations like addition, subtraction ,multiplication and remainder.c
@@ -0,0 +1,30 @@
+// Title of program
+// Program for arithmetic operations like addition,subtraction,multiplication and remainder
+//Header file
+#include<stdio.h>
+//function heading
+void main()
+{
+// variable declaration
+ int a,b,c;
+// output statement
+ printf("Enter two numbers");
+//Input statement 
+ scanf("%d%d",&a,&b);
+// Input statement for addition
+ c=a+b;
+//output statement
+ printf("sum=%d\n",c);
+//Input statement for subtraction
+ c=a-b;
+//output statement 
+ printf("difference=%d\n",c);
+// Input statement for multiplication
+ c=a*b;
+//output staement
+ printf("product=%d\n",c);
+// Input statement for division
+ c=a/b;
+// output statement
+ printf("divide=%d\n",c);
+}   
